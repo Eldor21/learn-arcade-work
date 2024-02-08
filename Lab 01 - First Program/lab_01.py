ab36569fe4766bbf07215ec32c7fab779e8914b9
Eldor21 <abdukhakimoe@hartwick.edu>
2024-02-08 09:40:37 -0500

    Update lab_01.py

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,14 +1,8 @@
-Hello, Barcelonaprint("Hello, Barcelona")
-print("Hello, Barcelona")
-print("Why Barcelona instead of World.")
 print("hello Zarafshan")
 print("Why Zarafshan instead of World.")
 print()
 
-print("Hello, Barcelona\nWhy Barcelona\tinstead of World.")
-print("Hello, Zarafshan\nWhy Barcelona\tinstead of World.")
 
-print()
 print()
 print("""Isn't it lovely, all alone?
 Heart made of glass, my mind of stone
