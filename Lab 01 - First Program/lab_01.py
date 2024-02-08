baa3013fd4321ef60abd24671cfeec62c45cd51c
Eldor21 <abdukhakimoe@hartwick.edu>
2024-02-08 09:37:14 -0500

    Update lab_01.py

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,9 +1,12 @@
 Hello, Barcelonaprint("Hello, Barcelona")
 print("Hello, Barcelona")
 print("Why Barcelona instead of World.")
+print("hello Zarafshan")
+print("Why Zarafshan instead of World.")
 print()
 
 print("Hello, Barcelona\nWhy Barcelona\tinstead of World.")
+print("Hello, Zarafshan\nWhy Barcelona\tinstead of World.")
 
 print()
 print()
