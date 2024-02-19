2330c7c3b3b8f4603949cb6a5bad1f4ea65c9098
Eldor21 <abdukhakimoe@hartwick.edu>
2024-02-19 08:41:39 -0500

    Update test.py

--- Testing/test.py
+++ Testing/test.py
@@ -1,10 +1,8 @@
-def print_hello():
-    """"this is a comment that describes a function"""
-    print("Hello!")
-def sum_two_numbers(a, b):
-    result = a + b
-    return result
+def f(x):
+    x += 1
+    print(x)
 
 
-result = sum_two_numbers(22,18)
-print(result)
+x =10
+f(x)
+print(x)
