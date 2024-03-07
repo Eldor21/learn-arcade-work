b410906c9b4d750a658029faa19eb902fe253661
Eldor21 <abdukhakimoe@hartwick.edu>
2024-03-07 09:07:55 -0500

    Update lab_04.py

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -4,20 +4,20 @@ import random
 def main():
   done = False
   miles_traveled = 0
-  thirst = 0
-  camel_tiredness = 0
-  distance_natives = -20
-  drinks_in_canteen = 3
+  hunger = 0
+  horse_tiredness = 0
+  distance_zombies = -20
+  foods_in_bag = 3
 
   while not done:
     random_num = random.randint(7, 14)
     random_move = random.randint(10, 20)
     random_go_ahead = random.randint(5, 12)
-    random_camel_tiredness = random.randint(1, 3)
+    random_horse_tiredness = random.randint(1, 3)
     random_oasis = random.randint(1, 20)
 
     print(
-        "\nA. Drink from your canteen.\nB. Ahead moderate speed.\nC. Ahead full speed.\nD. Stop for the night.\nE. Status check.\nQ. Quit."
+        "\nA. Eat from your bag.\nB. Ahead half of speed.\nC. Ahead full speed.\nD. Stop for rest in safe place.\nE. Status check.\nQ. Quit."
     )
     choice = input("What is your choice? ").upper()
 
@@ -27,57 +27,60 @@ def main():
 
     elif choice == "E":
       print(
-          f"\nMiles traveled: {miles_traveled}\nDrinks in canteen: {drinks_in_canteen}\nThirst level: {thirst}\nCamel Tiredness: {camel_tiredness}\nThe natives are {abs(miles_traveled - distance_natives)} miles behind you.\n"
+          f"\nMiles traveled: {miles_traveled}\nFoods in bag: {foods_in_bag}\nHunger level: {hunger}\nHorse Tiredness: {horse_tiredness}\nThe zombies are {abs(miles_traveled - distance_zombies)} miles behind you.\n"
       )
 
     elif choice == "D":
-      camel_tiredness = 0
-      print("\nCamel is happy!\n")
-      distance_natives += random_num
+      horse_tiredness = 0
+      print("\nHorse is happy!\n")
+      distance_zombies += random_num
 
     elif choice == "C":
       miles_traveled += random_move
       print(f"\nYou traveled {random_move} miles\n")
-      thirst += 1
-      camel_tiredness += random_camel_tiredness
-      distance_natives += random_num
+      hunger += 1
+      horse_tiredness += random_horse_tiredness
+      distance_zombies += random_num
 
     elif choice == "B":
       miles_traveled += random_go_ahead
       print(f"\nYou traveled {random_go_ahead} miles\n")
-      thirst += 1
-      camel_tiredness += 1
-      distance_natives += random_num
+      hunger += 1
+      horse_tiredness += 1
+      distance_zombies += random_num
 
     elif choice == "A":
-      if drinks_in_canteen > 0:
-        drinks_in_canteen -= 1
-        thirst = 0
-        print("\nYou drank from your canteen.\n")
+      if foods_in_bag > 0:
+        foods_in_bag -= 1
+        hunger = 0
+        print("\nYou ate food from your bag.\n")
       else:
-        print("\nYou have no water left!\n")
+        print("\nYou have no food left!\n")
 
-    if thirst > 4:
-      print("\nYou are thirsty!\n")
-    if camel_tiredness > 5:
-      print("\nYour camel is getting tired!\n")
-
+    if hunger > 4:
+      print("\nYou are hungry!\n")
+      done = True
+      print("You starved to death")
+    if horse_tiredness > 5:
+      print("\nYour horse is getting tired!\n")
+      done = True
+      print("Your horse died of exhaustion")
     if miles_traveled >= 200:
       done = True
-      print("\nYou won! Congratulations!\n")
+      print("\nCongratulations! You survived\n")
 
     if random_oasis == 17:
       print(
-          "\nYou found an oasis! Your canteen is refilled, and your camel is rested.\n"
+          "\nYou found an oasis! Your bag is refilled, and your horse is rested.\n"
       )
-      drinks_in_canteen = 3
-      camel_tiredness = 0
+      foods_in_bag = 3
+      horse_tiredness = 0
 
-    if distance_natives >= miles_traveled:
-      print("\nThe natives caught up to you!\n")
+    if distance_zombies >= miles_traveled:
+      print("\nThe zombies caught up to you!\n")
       done = True
-    elif distance_natives + 15 > miles_traveled:
-      print("\nThe natives are getting closer!\n")
+    elif distance_zombies + 14 > miles_traveled:
+      print("\nHurry up, The zombies are getting closer!\n")
 
   print("\nGame Over\n")
 
