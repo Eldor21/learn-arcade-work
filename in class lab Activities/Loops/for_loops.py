5f82b2137e6b53b0ff75373be58695f9483eddf3
Eldor21 <abdukhakimoe@hartwick.edu>
2024-05-02 08:55:32 -0400

    hi

--- in class lab Activities/Loops/for_loops.py
+++ in class lab Activities/Loops/for_loops.py
@@ -1,77 +1,88 @@
-# 11.5.1.1
+#problem 1
 for i in range(10):
-    print("Hi")
-
-# 11.5.1.2
+    print("*", end=" ")
+print("\n")
+#problem 2
+for i in range(10):
+    print("*", end=" " )
+print("")
 for i in range(5):
-    print("Hello")
-print("There")
-
-# 11.5.1.3
-for i in range(5):
-    print("Hello,")
-    print("There")
-
-# 11.5.1.4
+    print("*", end=" ")
+print()
+for i in range(20):
+        print("*", end=" ")
+print("\n")
+#problem 3
+for row in range(10):
+    for column in range(10):
+        print("*", end=" ")
+    print("")
+#problem 4
+for row in range(10):
+    for column in range(5):
+        print("*", end=" ")
+    print("")
+#problem 5
+for row in range(5):
+    for column in range(20):
+        print("*", end=" ")
+    print("")
+#problem 6
 for i in range(10):
-    print(i)
-
-# 11.5.1.5
-for i in range(1, 11):
-    print(i)
-
-# Another way Print the numbers 1 to 10.
-for i in range(10):
-    # Add one to i, just before printing
-    print(i + 1)
-
-# 11.5.1.6
-# One way to print the even numbers 2 to 10
-for i in range(2, 12, 2):
-    print(i)
-
-# Another way to print the numbers 2 to 10
-for i in range(5):
-    print((i + 1) * 2)
-
-# 11.5.1.7
-for i in range(10, 0, -1):
-    print(i)
-
-# 11.5.1.8
-for item in [2, 6, 4, 2, 4, 6, 7, 4]:
-    print(item)
-
-# 11.5.1.9
-for i in range(3):
-    print("a")
-    for j in range(3):
-        print("b")
-
-# 11.5.1.10
-a = 0
-for i in range(10):
-    a = a + 1
-print(a)
-
-# 11.5.1.11
-a = 0
-for i in range(10):
-    a = a + 1
-for j in range(10):
-    a = a + 1
-print(a)
-
-# 11.5.1.12
-a = 0
-for i in range(10):
-    a = a + 1
     for j in range(10):
-        a = a + 1
-print(a)
+        print(j, end=" ")
+    print("")
+#problem 7
+for i in range(10):
+    for j in range(10):
+        print(i, end=" ")
+    print("")
+#problem 8
+for i in range(10):
+    for j in range(i + 1):
+        print(i, end=" ")
+    print("")
+#problem 9
+for i in range(10):
+    for j in range(i):
+        print(" ", end=" ")
+    for j in range(10 - i):
+        print(j, end=" ")
+    print()
 
-# 11.5.1.13
-total = 0
-for i in range(1, 101):
-    total += i
-print(total)
+for i in range(10):
+    for j in range(10-i):
+        print(j, end=" ")
+    print()
+
+#problem 10
+
+for i in range(1, 10):
+    for j in range(1, 10):
+        if i * j < 10:
+            print(" ", end="")
+
+        print(i * j, end=" ")
+
+    print()
+
+for i in range(1, 13):
+    for j in range(1, 13):
+        if i * j < 100:
+            print(" ", end="")
+        if i * j < 10:
+            print(" ", end="")
+
+        print(i * j, end=" ")
+
+    print()
+
+#problem 11
+for i in range(10):
+    for j in range(10 - i):
+        print (" ", end=" ")
+    for j in range(1, i + 1):
+        print (j, end=" ")
+    for j in range(i - 1, 0, -1):
+        print (j, end=" ")
+    print()
